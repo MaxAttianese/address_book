16c9c29d4a2bc863d04ddd14fc8752813670b342
maxattianese <maxattianesekr94@gmail.com>
2024-01-05 21:55:47 +0100

    Add table view in smartphone screen size

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const btn = document.getElementById("changeStyleMode");
+const btnSwitchColor = document.getElementById("changeStyleMode");
+const btnSwitchView = document.getElementById("changeViewMode");
+
 
 const titleContainer = document.getElementsByClassName("title-container");
 const iconContainer = document.getElementsByClassName("icon-container");
 const tableBorder = document.getElementsByClassName("table-border");
 const tBody = document.getElementsByClassName("tbody-light-mode");
 const circles = document.getElementsByClassName("circle-id");
+const cardsPeople = document.getElementsByClassName("card-people");
+const linkContainer = document.getElementsByClassName("link-container");
 
-btn.addEventListener("click", () => {
+
+btnSwitchColor.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
     titleContainer[0].classList.toggle("title-container-dark-mode");
     iconContainer[0].classList.toggle("icon-container-dark-mode");
@@ -16,5 +21,15 @@ btn.addEventListener("click", () => {
         circles[i].classList.toggle("circle-id-dark-mode");
         
     }
-    btn.classList.toggle("btn-dark");
+    for (let i = 0; i < cardsPeople.length; i++) {
+        cardsPeople[i].classList.toggle("card-people-dark");
+        
+    }
+    for (let i = 0; i < linkContainer.length; i++) {
+        linkContainer[i].classList.toggle("link-container-dark");
+        
+    }
+    btnSwitchColor.classList.toggle("btn-dark");
+    btnSwitchView.classList.toggle("btn-dark");
 })
+
