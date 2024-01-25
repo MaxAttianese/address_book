a5b0be79532b8521168daa6d22cbb65cee80dd3f
maxattianese <maxattianesekr94@gmail.com>
2024-01-25 12:37:17 +0100

    Added all changes that TL required

--- people/js/people.js
+++ people/js/people.js
@@ -1,5 +1,7 @@
 const btnSwitchColor = document.getElementById("changeStyleMode");
 
+const titleContainer = document.getElementsByClassName("title-container");
+const iconContainer = document.getElementsByClassName("icon-container");
 const cardContainer = document.getElementsByClassName("card-container");
 const personalImg = document.getElementsByClassName("personal-img");
 const divLink = document.getElementsByClassName("bg-a-dark");
@@ -9,6 +11,8 @@ btnSwitchColor.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     cardContainer[0].classList.toggle("card-container-dark");
     personalImg[0].classList.toggle("personal-img-dark");
+    titleContainer[0].classList.toggle("title-container-dark");
+    iconContainer[0].classList.toggle("icon-container-dark");
     for (let i = 0; i < divLink.length; i++) {
         divLink[i].classList.toggle("bg-address-a-dark");
     }
